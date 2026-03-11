448b67af021ce35de4d2c7d502841a46b6cf5325
Naji <115167583+najix2ae@users.noreply.github.com>
2026-03-11 23:14:18 +0300

    Update recipe images to local paths

--- script.js
+++ script.js
@@ -3,36 +3,36 @@
 // ================================
 
 const recipes = [
-  { name:"Spaghetti Carbonara", type:"italian", time:"25 min", diff:"Medium", img:"images/spaghetti_carbonara.jpg" },
-  { name:"Lasagna", type:"italian", time:"1 hr", diff:"Hard", img:"images/lasagna.jpg" },
-  { name:"Margherita Pizza", type:"italian", time:"30 min", diff:"Medium", img:"images/margherita_pizza.jpg" },
-  { name:"Tiramisu", type:"italian", time:"45 min", diff:"Medium", img:"images/tiramisu.jpg" },
-  { name:"Sushi Rolls", type:"japanese", time:"50 min", diff:"Hard", img:"images/sushi_rolls.jpg" },
-  { name:"Ramen", type:"japanese", time:"40 min", diff:"Medium", img:"images/ramen.jpg" },
-  { name:"Tempura", type:"japanese", time:"20 min", diff:"Easy", img:"images/tempura.jpg" },
-  { name:"Tacos al Pastor", type:"mexican", time:"20 min", diff:"Medium", img:"images/tacos_al_pastor.jpg" },
-  { name:"Quesadilla", type:"mexican", time:"10 min", diff:"Easy", img:"images/quesadilla.jpg" },
-  { name:"Burrito", type:"mexican", time:"20 min", diff:"Easy", img:"images/burrito.jpg" },
-  { name:"Churros", type:"mexican", time:"25 min", diff:"Medium", img:"images/churros.jpg" },
-  { name:"Cheeseburger", type:"american", time:"20 min", diff:"Easy", img:"images/cheeseburger.jpg" },
-  { name:"Fried Chicken", type:"american", time:"30 min", diff:"Medium", img:"images/fried_chicken.jpg" },
-  { name:"Mac and Cheese", type:"american", time:"25 min", diff:"Easy", img:"images/mac_and_cheese.jpg" },
-  { name:"Caesar Salad", type:"lunch", time:"12 min", diff:"Easy", img:"images/caesar_salad.jpg" },
-  { name:"Pancakes", type:"breakfast", time:"15 min", diff:"Easy", img:"images/pancakes.jpg" },
-  { name:"Omelette", type:"breakfast", time:"10 min", diff:"Easy", img:"images/omelette.jpg" },
-  { name:"French Toast", type:"breakfast", time:"12 min", diff:"Easy", img:"images/french_toast.jpg" },
-  { name:"Smoothie Bowl", type:"breakfast", time:"8 min", diff:"Easy", img:"images/smoothie_bowl.jpg" },
-  { name:"Grilled Salmon", type:"dinner", time:"30 min", diff:"Medium", img:"images/grilled_salmon.jpg" },
-  { name:"Chocolate Cake", type:"dessert", time:"50 min", diff:"Hard", img:"images/chocolate_cake.jpg" },
-  { name:"Ice Cream Sundae", type:"dessert", time:"5 min", diff:"Easy", img:"images/ice_cream_sundae.jpg" },
-  { name:"Brownies", type:"dessert", time:"35 min", diff:"Medium", img:"images/brownies.jpg" },
-  { name:"Butter Chicken", type:"indian", time:"40 min", diff:"Medium", img:"images/butter_chicken.jpg" },
-  { name:"Chicken Biryani", type:"indian", time:"1 hr", diff:"Hard", img:"images/chicken_biryani.jpg" },
-  { name:"Paneer Tikka", type:"indian", time:"30 min", diff:"Medium", img:"images/paneer_tikka.jpg" },
-  { name:"Masala Dosa", type:"indian", time:"40 min", diff:"Medium", img:"images/masala_dosa.jpg" },
-  { name:"Pad Thai", type:"thai", time:"25 min", diff:"Medium", img:"images/pad_thai.jpg" },
-  { name:"Tom Yum Soup", type:"thai", time:"20 min", diff:"Medium", img:"images/tom_yum_soup.jpg" },
-  { name:"Green Curry", type:"thai", time:"35 min", diff:"Medium", img:"images/green_curry.jpg" }
+  { name:"Spaghetti Carbonara", type:"italian", time:"25 min", diff:"Medium", img:"spaghetti_carbonara.jpg" },
+  { name:"Lasagna", type:"italian", time:"1 hr", diff:"Hard", img:"lasagna.jpg" },
+  { name:"Margherita Pizza", type:"italian", time:"30 min", diff:"Medium", img:"margherita_pizza.jpg" },
+  { name:"Tiramisu", type:"italian", time:"45 min", diff:"Medium", img:"tiramisu.jpg" },
+  { name:"Sushi Rolls", type:"japanese", time:"50 min", diff:"Hard", img:"sushi_rolls.jpg" },
+  { name:"Ramen", type:"japanese", time:"40 min", diff:"Medium", img:"ramen.jpg" },
+  { name:"Tempura", type:"japanese", time:"20 min", diff:"Easy", img:"tempura.jpg" },
+  { name:"Tacos al Pastor", type:"mexican", time:"20 min", diff:"Medium", img:"tacos_al_pastor.jpg" },
+  { name:"Quesadilla", type:"mexican", time:"10 min", diff:"Easy", img:"quesadilla.jpg" },
+  { name:"Burrito", type:"mexican", time:"20 min", diff:"Easy", img:"burrito.jpg" },
+  { name:"Churros", type:"mexican", time:"25 min", diff:"Medium", img:"churros.jpg" },
+  { name:"Cheeseburger", type:"american", time:"20 min", diff:"Easy", img:"cheeseburger.jpg" },
+  { name:"Fried Chicken", type:"american", time:"30 min", diff:"Medium", img:"fried_chicken.jpg" },
+  { name:"Mac and Cheese", type:"american", time:"25 min", diff:"Easy", img:"mac_and_cheese.jpg" },
+  { name:"Caesar Salad", type:"lunch", time:"12 min", diff:"Easy", img:"caesar_salad.jpg" },
+  { name:"Pancakes", type:"breakfast", time:"15 min", diff:"Easy", img:"pancakes.jpg" },
+  { name:"Omelette", type:"breakfast", time:"10 min", diff:"Easy", img:"omelette.jpg" },
+  { name:"French Toast", type:"breakfast", time:"12 min", diff:"Easy", img:"french_toast.jpg" },
+  { name:"Smoothie Bowl", type:"breakfast", time:"8 min", diff:"Easy", img:"smoothie_bowl.jpg" },
+  { name:"Grilled Salmon", type:"dinner", time:"30 min", diff:"Medium", img:"grilled_salmon.jpg" },
+  { name:"Chocolate Cake", type:"dessert", time:"50 min", diff:"Hard", img:"chocolate_cake.jpg" },
+  { name:"Ice Cream Sundae", type:"dessert", time:"5 min", diff:"Easy", img:"ice_cream_sundae.jpg" },
+  { name:"Brownies", type:"dessert", time:"35 min", diff:"Medium", img:"brownies.jpg" },
+  { name:"Butter Chicken", type:"indian", time:"40 min", diff:"Medium", img:"butter_chicken.jpg" },
+  { name:"Chicken Biryani", type:"indian", time:"1 hr", diff:"Hard", img:"chicken_biryani.jpg" },
+  { name:"Paneer Tikka", type:"indian", time:"30 min", diff:"Medium", img:"paneer_tikka.jpg" },
+  { name:"Masala Dosa", type:"indian", time:"40 min", diff:"Medium", img:"masala_dosa.jpg" },
+  { name:"Pad Thai", type:"thai", time:"25 min", diff:"Medium", img:"pad_thai.jpg" },
+  { name:"Tom Yum Soup", type:"thai", time:"20 min", diff:"Medium", img:"tom_yum_soup.jpg" },
+  { name:"Green Curry", type:"thai", time:"35 min", diff:"Medium", img:"green_curry.jpg" }
 ];
 
 // ================================
@@ -147,4 +147,5 @@ if(filter){
 // ================================
 function toggleDark(){
   document.body.classList.toggle("dark");
+
 }
